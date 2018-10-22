2108cd9712f7a212395a3bc05fa4c82bfdc5351e
MDecker-MobileComputing <mide_42b@yahoo.de>
2018-10-22 08:44:31 +0200

    Header-Kommentar verbessert

--- app/src/main/java/de/mide/ressourcen_demo/NonMainActivities/SprachanpassungsActivity.java
+++ app/src/main/java/de/mide/ressourcen_demo/NonMainActivities/SprachanpassungsActivity.java
@@ -7,14 +7,17 @@ import de.mide.ressourcen_demo.R;
 
 
 /**
- * Activity demonstriert Anpassung von Texten und auch Grafiken
- * an verschiedene Sprachen.
+ * Activity demonstriert Anpassung von Texten und Grafiken
+ * an verschiedene Sprachen (Internationalization, i18n).
  * <br><br>
  *
  * This project is licensed under the terms of the BSD 3-Clause License.
  */
 public class SprachanpassungsActivity extends Activity {
 
+    /**
+     * Lifecycle-Methode, lädt Layout-Datei.
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
